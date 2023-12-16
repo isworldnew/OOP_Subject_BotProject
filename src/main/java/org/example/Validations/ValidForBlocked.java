ea7b871929ae05b8b179d2d49d6d337d66780dd9
isworldnew <isworldnew@mail.ru>
2023-12-16 19:54:47 +0400

    Validatable objects now have a specific implementation

--- src/main/java/org/example/Validations/ValidForBlocked.java
+++ src/main/java/org/example/Validations/ValidForBlocked.java
@@ -1,4 +1,31 @@
 package org.example.Validations;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ValidForBlocked implements Validatable{
+    @Override
+    public boolean isValid(String text) {
+
+        //проверяем, действительно ли текст сообщения соответствует
+        //маске: LIKE ('[А-Я][0-9][0-9][0-9][А-Я][А-Я][0-9][0-9]')
+        //OR LIKE ('[А-Я][0-9][0-9][0-9][А-Я][А-Я][0-9][0-9][0-9]')
+
+        if (text.length() == 8) {
+            Pattern numberPlatePattern = Pattern.compile("[А-Я][0-9][0-9][0-9][А-Я][А-Я][0-9][0-9]");
+            Matcher checkPattern = numberPlatePattern.matcher(text.toUpperCase());
+
+            if (checkPattern.find()) return true;
+        }
+
+        if (text.length() == 9) {
+            Pattern numberPlatePattern = Pattern.compile("[А-Я][0-9][0-9][0-9][А-Я][А-Я][0-9][0-9][0-9]");
+            Matcher checkPattern = numberPlatePattern.matcher(text.toUpperCase());
+
+
+            if (checkPattern.find()) return true;
+        }
+
+        return false;
+    }
 }
--- src/main/java/org/example/Validations/ValidForWBF.java
+++ src/main/java/org/example/Validations/ValidForWBF.java
@@ -1,4 +1,46 @@
 package org.example.Validations;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ValidForWBF implements Validatable {
+    private static List<String> streets = new ArrayList<>();
+
+    static {
+        streets.add("kfc");
+        streets.add("беговая");
+        streets.add("политехническая");
+        streets.add("миротворцева");
+        streets.add("большая садовая");
+        streets.add("2-я садовая");
+    }
+
+    private String[] dataFromText;
+
+    private Pattern timePattern = Pattern.compile("[0-9][0-9][:][0-9][0-9]");
+    private ValidForBlocked vfb = new ValidForBlocked();
+
+    @Override
+    public boolean isValid(String text) {
+        this.dataFromText = text.split(", ");
+
+        if (!streets.contains(dataFromText[0].toLowerCase()))
+            return false;
+
+        Matcher timeMatch = timePattern.matcher(dataFromText[1]);
+
+        if (!timeMatch.find())
+            return false;
+
+        if (!vfb.isValid(dataFromText[5]))
+            return false;
+
+        return true;
+    }
+
+    public String[] getMessageData() {
+        return this.dataFromText;
+    }
 }
