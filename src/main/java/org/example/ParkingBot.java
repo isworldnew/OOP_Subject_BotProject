bce2e4071e908f060a82d3acf561cc8031281ebd
isworldnew <isworldnew@mail.ru>
2023-12-16 19:50:52 +0400

    Commands' actions are partly described now

--- src/main/java/org/example/ParkingBot.java
+++ src/main/java/org/example/ParkingBot.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -9,8 +11,10 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import org.example.db.LocalDataBaseInteraction;
 
+import java.io.File;
 import java.sql.Connection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -59,6 +63,51 @@ public class ParkingBot extends TelegramLongPollingBot {
         String text = update.getMessage().getText();
         long chatId = update.getMessage().getChatId();
 
+        if (text.equals(Command.START.commandText)) {
+            db.registerNewChatId(chatId);
+        } //ready
+
+        if (text.equals(Command.INFO.commandText)) {
+            sendMessageToChat(chatId, Command.INFO.commandText);
+        } //ready
+
+        if (text.equals(Command.WILLBEFREE.commandText)) {
+
+        }
+
+        if (text.equals(Command.WHERETOPARK.commandText)) {
+
+        }
+
+        if (text.equals(Command.TRAFFICPOLICEALARM.commandText)) {
+            List<Long> allChatId = db.getAllChatId();
+
+            for (Long currentChatId : allChatId)
+                sendMessageToChat(currentChatId, Command.TRAFFICPOLICEALARM.commandText);
+
+        } //ready?
+
+        if (text.equals(Command.BLOCKED.commandText)) {
+
+        }
+
+        if (text.equals(Command.DELETEMETA.commandText)) {
+            db.deleteMeta(chatId);
+        } //ready
+
+        if (text.equals(Command.PLACES.commandText)) {
+            File photo = new File("D:\\OOP_Project\\map.png");
+
+            SendPhoto sendPhoto = new SendPhoto();
+            sendPhoto.setChatId(chatId);
+            sendPhoto.setPhoto(new InputFile(photo));
+
+            try { execute(sendPhoto); }
+            catch (TelegramApiException ex) {
+                System.out.println("ну ладно, сам разберётся");
+            }
+        } //ready
+
     }
 
     public void handleUserDataInput(Update update) {}
@@ -67,10 +116,11 @@ public class ParkingBot extends TelegramLongPollingBot {
         SendMessage msg = new SendMessage();
         msg.setChatId(chatId);
         msg.setText(text);
+        msg.enableHtml(true);
 
         try { execute(msg); }
         catch(TelegramApiException ex) {
-            System.out.println("telegram api exception");
+            System.out.println("не отправилось");
         }
     }
 
